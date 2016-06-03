9c3fe282afbe595e6dea161ea0196fde16bfb3b1
mengqiqi0224 <mengqiqi0224@gmail.com>
2016-06-03 09:18:31 -0400

    add link

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :immunizations
   resources :medications
   resources :patient_medications
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routedes".
 
