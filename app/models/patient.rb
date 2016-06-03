4b0ba83e7e1183afcf1d4383d00d2af4a6ece068
mesut <mesut@GuXixideMacBook-Pro.local>
2016-06-03 15:10:54 -0400

    version three

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,10 +1,9 @@
 class Patient < ActiveRecord::Base
-<<<<<<< HEAD
-=======
+
   has_many :immunizations
   has_many :patient_medications
   has_many :appointments
   has_many :providers, through: :appointments
   belongs_to :user
->>>>>>> 53c800659cbf8b39a02eb3f95211a9eb2c7dab1b
+
 end
