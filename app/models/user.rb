e2b98b9f3ae531855ee0589358c2653083c18363
mengqiqi0224 <mengqiqi0224@gmail.com>
2016-06-03 22:32:40 -0400

    half-finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,  :lockable, :timeoutable ,:omniauthable
+         :recoverable, :rememberable, :trackable, :validatable
   has_one :patient
   has_one :provider
 end
